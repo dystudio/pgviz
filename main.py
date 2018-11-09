fe69a97181c4b73bcc86ff99abe1812e56660c8d
Eko Edita <ekoeditaa@gmail.com>
2018-11-09 14:31:12 +0800

    [MainFunction] Use functions from other files now instead of built-in psycopg2 API

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import psycopg2
+from db_credentials import DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD
+from db_connection import get_execution_plan, connect_to_db
 
-conn = psycopg2.connect('dbname=project2 user=postgres password=root')
+conn = connect_to_db(DATABASE_NAME, DATABASE_USER, DATABASE_PASSWORD)
 cur = conn.cursor()
-cur.execute("EXPLAIN (FORMAT JSON) SELECT * FROM author LIMIT 10;")
+final_query = get_execution_plan("SELECT * FROM author LIMIT 10;")
+cur.execute(final_query)
 print(cur.fetchall())
